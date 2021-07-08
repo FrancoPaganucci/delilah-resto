c373cf23336956b259f95e78f0836f8e942e459e
FrancoPaganucci <70669328+FrancoPaganucci@users.noreply.github.com>
2021-07-08 14:47:39 -0300

    Add files via upload

--- models/pedidos-has-platos.js
+++ models/pedidos-has-platos.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const { DataTypes } = require('sequelize');
 const sequelize = require('./../config');
 const Pedidos =require("./pedidos");
@@ -35,42 +34,5 @@ const PedidosHasPlatos = sequelize.define('PedidosHasPlatos', {
     sequelize
 })
 
-=======
-const { DataTypes } = require('sequelize');
-const sequelize = require('./../config');
-const Pedidos =require("./pedidos");
-const Platos = require('./platos');
 
-
-const PedidosHasPlatos = sequelize.define('PedidosHasPlatos', {
-    pedidos_id: {
-        field: "pedidos_id",
-        type: DataTypes.INTEGER,
-        notNull: true,
-        references: {
-            model: Pedidos,
-            key: "id"
-        }
-    },
-    platos_id: {
-        field: "platos_id",
-        type: DataTypes.INTEGER,
-        notNull: true,
-        references: {
-            model: Platos,
-            key: "id"
-        }
-    },
-    cantidad: {
-        type: DataTypes.INTEGER,
-        notNull: true
-    }
-}, {
-    timestamps: false,
-    tableName: "pedidos_has_platos",
-    underscored: true,
-    sequelize
-})
-
->>>>>>> 6cc2c03532de7cf629d10a5893962c69203215a5
 module.exports = PedidosHasPlatos;
--- models/platos.js
+++ models/platos.js
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-<<<<<<<< HEAD:models/platos.js
 const { DataTypes } = require('sequelize');
 const sequelize = require('../config');
 
@@ -28,51 +26,3 @@ const Platos = sequelize.define('platos', {
 })
 
 module.exports = Platos;
-========
-const { DataTypes } = require('sequelize');
-const sequelize = require('..');
-
-const Plato = sequelize.define('plato', {
-=======
-const { DataTypes } = require('sequelize');
-const sequelize = require('../config');
-
-const Platos = sequelize.define('platos', {
->>>>>>> 6cc2c03532de7cf629d10a5893962c69203215a5
-    nombre: {
-        type: DataTypes.STRING,
-        notNull: true
-    },
-    precio: {
-        type: DataTypes.DOUBLE,
-        notNull: true
-    },
-    activo: {
-<<<<<<< HEAD
-        type: DataTypes.TINYINT,
-=======
-        type: DataTypes.BOOLEAN,
->>>>>>> 6cc2c03532de7cf629d10a5893962c69203215a5
-        notNull: true
-    },
-    imagen: {
-        type: DataTypes.STRING,
-        notNull: true
-    },
-
-}, {
-    timestamps: false,
-    tableName: "platos",
-<<<<<<< HEAD
-    modelName: "plato",
-    sequelize
-})
-
-module.exports = Plato;
->>>>>>>> 6cc2c03532de7cf629d10a5893962c69203215a5:db/models/plato.js
-=======
-    underscored: true
-})
-
-module.exports = Platos;
->>>>>>> 6cc2c03532de7cf629d10a5893962c69203215a5
